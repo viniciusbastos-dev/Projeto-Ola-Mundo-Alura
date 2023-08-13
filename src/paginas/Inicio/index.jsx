01dbed4cd32fc585134bb1640edbabae1ccc8a18
Vinícius Bastos <viniciusbsilva57@gmail.com>
2023-08-13 19:15:52 -0300

    Create Post and Rodape component

--- src/paginas/Inicio/index.jsx
+++ src/paginas/Inicio/index.jsx
@@ -1,10 +1,20 @@
 import Banner from "componentes/Banner";
+import styles from "./Inicio.module.css";
+
+import posts from "json/posts.json";
+import Post from "componentes/Post";
 
 export default function Inicio() {
     return (
         <main>
             <Banner />
-            <h1>Olá, mundo!</h1>
+            <ul className={styles.posts}>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Post post={post}/>
+                    </li>
+                ))}
+            </ul>
         </main>
     );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
 
 function AppRoutes() {
     return (
@@ -13,6 +14,7 @@ function AppRoutes() {
                 <Route path="/sobremim" element={<SobreMim />} />
                 <Route path="*" element={<div>Página não encontrada!</div>} />
             </Routes>
+            <Rodape />
         </BrowserRouter>
     );
 }
