0c83e5fa77d5d831811318ad86aedcf65b0d22e5
Vinícius Bastos <viniciusbsilva57@gmail.com>
2023-08-14 14:54:20 -0300

    Create function to return to the previous page in 404 page

--- src/componentes/BotaoPrincipal/index.jsx
+++ src/componentes/BotaoPrincipal/index.jsx
@@ -1,5 +1,5 @@
 import styles from "./BotaoPrincipal.module.css";
 
-export default function BotaoPrincipal({ children }) {
-    return <button className={styles.botaoPrincipal}>{children}</button>;
+export default function BotaoPrincipal({ children, tamanho }) {
+    return <button className={`${styles.botaoPrincipal} ${styles[tamanho]}`}>{children}</button>;
 }
--- src/paginas/NaoEncontrada/index.jsx
+++ src/paginas/NaoEncontrada/index.jsx
@@ -1,7 +1,11 @@
+import BotaoPrincipal from "componentes/BotaoPrincipal";
 import styles from "./NaoEncontrada.module.css";
 import erro404 from "assets/erro_404.png";
+import { useNavigate } from "react-router-dom";
 
 export default function NaoEncontrada() {
+    const navegar = useNavigate();
+
     return (
         <>
             <div className={styles.conteudoContainer}>
@@ -18,8 +22,11 @@ export default function NaoEncontrada() {
                     página inicial.
                 </p>
 
-                <div className={styles.botaoContainer}>
-                    <button>Voltar</button>
+                <div
+                    className={styles.botaoContainer}
+                    onClick={() => navegar(-1)}
+                >
+                    <BotaoPrincipal tamanho="lg">Voltar</BotaoPrincipal>
                 </div>
 
                 <img
