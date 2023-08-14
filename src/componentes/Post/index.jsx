2e5e7b92edb778fe68fa45679f831d092d4ef3d2
Vinícius Bastos <viniciusbsilva57@gmail.com>
2023-08-14 14:19:08 -0300

    Change Post to PostCard and Create new Post component

--- src/componentes/Post/index.jsx
+++ src/componentes/Post/index.jsx
@@ -1,16 +1,22 @@
-import styles from "./Post.module.css";
+import { useParams } from "react-router-dom";
+import posts from "json/posts.json";
+import PostModelo from "componentes/PostModelo";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import "./Post.css";
 
-export default function Post({ post }) {
+export default function Post() {
+    const parametros = useParams();
+
+    const post = posts.find((post) => post.id === Number(parametros.id));
+
     return (
-        <div className={styles.post}>
-            <img
-                className={styles.capa}
-                src={`/assets/posts/${post.id}/capa.png`}
-                alt="Imagem de capa do post"
-            />
-            <h2 className={styles.titulo}>{post.titulo}</h2>
-
-            <button className={styles.botaoLer}>Ler</button>
-        </div>
+        <PostModelo
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+        >
+            <div className="post-markdown-container">
+                <ReactMarkdown>{post.texto}</ReactMarkdown>
+            </div>
+        </PostModelo>
     );
 }
